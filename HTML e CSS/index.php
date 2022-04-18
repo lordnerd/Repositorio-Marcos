7c4f26ad446aced02a4fc58477093c8c970667e0
lordnerd <vinnycfreitas1@gmail.com>
2022-04-18 13:08:06 -0300

    exclui o arquivo teste.txt e adiciona os arquivos php index e index2 a pasta HTML e CSS

---
+++ HTML e CSS/index.php
@@ -0,0 +1,18 @@
+<html>
+
+	<head>
+		<title>Primeiro site mermão! HARDCORE!</title>
+	</head>
+	
+	<body>
+	
+		<?php
+			for ($i = 0; $i<10; $i++){
+				print("Linha número" . $i . "<br />");
+			}
+		?>
+		
+	</body>
+	
+</html>
+		
---
+++ HTML e CSS/index2.php
@@ -0,0 +1,36 @@
+<html>
+
+	<head>
+		<title>Primeiro site mermão! HARDCORE!</title>
+		
+		<script src= "https://code.jquery.com/jquery-3.6.0.min.js"></script>
+		
+		<style  type="text/css">
+			.linha {
+				font-weight: bold;
+				color: blue;
+				padding-left: 10px;
+				line-height: 50px;
+			}
+			
+		</style>
+	</head>
+	
+	<body>
+	
+		<?php
+			for ($i = 0; $i < 10; $i++ ) {
+				print("<span class=\"linha\">Linha número " . $i . "</span><br />");
+			}
+		?>
+		
+	</body>
+	
+	<script type= "text/javascript">
+		$(document).ready(function() {
+			alert("Yes baby. Thank you!");
+		});
+		
+	</script>
+	
+</html>
