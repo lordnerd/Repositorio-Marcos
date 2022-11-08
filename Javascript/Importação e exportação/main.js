37ce7c0f5b4022916f3889f43cad6ea85e5c3088
lordnerd <vinnycfreitas1@gmail.com>
2022-11-08 16:52:34 -0300

    Adiciona testes com importação e exportação em Javascript

---
+++ Javascript/Importação e exportação/main.js
@@ -0,0 +1,17 @@
+
+const { gets, print } = require('./funções-auxiliares');
+
+const quantidadeDeAlunos = gets();
+let maiorValorEncontrado = 0;
+
+for (let i = 0; i < quantidadeDeAlunos; i++) {
+    const numeroSorteado = gets();
+    if(numeroSorteado > maiorValorEncontrado){
+        maiorValorEncontrado = numeroSorteado;
+    }
+}
+
+
+print(maiorValorEncontrado);
+
+
