f3861243ac389c46ae7c4f0f317451bc3576ec81
lordnerd <vinnycfreitas1@gmail.com>
2022-12-27 20:47:56 -0300

    Adiciona testes com a sintaxe Java

---
+++ Java-basics/Aprendendo a sintaxe Java/java-tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,17 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        
+        String meuNome = "Marcos";
+        System.out.println(meuNome);
+
+        double salarioMinimo = 1250.34;
+        salarioMinimo = 2000.56;
+
+        System.out.println(salarioMinimo);
+
+        final double PI = 3.14;
+
+        System.out.println(PI);
+
+    }
+}
